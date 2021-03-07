7faf43a88ed384ae7c6f71feafc5297ed25771ba
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2021-03-08 01:22:36 +0600

    Outputting Lists

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import React from 'react';
+
 import MyApp from './FirstApp'
 
 
-function App() {
-  return (
-    <div className="App">
-    <MyApp/>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    player: [
+      { name: 'jack', age: 23, id: '1' },
+      { name: 'ruku', age: 22, id: '2' },
+      { name: 'partho', age: 22, id: '3' },
+      { name: 'pritom', age: 23, id: '4' }
+    ]
+  }
+
+  render() {
+    return (
+      <div>
+        <MyApp players={this.state.player} />
+      </div>
+    );
+  }
+
 }
 
 export default App;
--- src/FirstApp.js
+++ src/FirstApp.js
@@ -1,12 +1,19 @@
 import React,{Component} from 'react'
 
 class MyApp extends Component {
+
     render() {
+        const {players} = this.props
+        const players_info = players.map(pl=>{
+            return(
+                <div className='App' key={pl.id}>
+                <h1>My Name is {pl.name}</h1>
+                <h2>I am {pl.age} years old</h2>
+              </div>
+            )
+        })
         return(
-            <div className='App'>
-              <h1>My Name is Jack</h1>
-              <h2>Learning React</h2>
-            </div>
+            <div> {players_info}</div>
         )
     }
 }
