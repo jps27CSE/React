69724e8f4c073e63367c7e8e9b996c117fa55ef2
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2021-03-08 00:35:43 +0600

    Nesting Components

---
+++ src/FirstApp.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react'
+
+class MyApp extends Component {
+    render() {
+        return(
+            <div className='App'>
+              <h1>My Name is Jack</h1>
+              <h2>Learning React</h2>
+            </div>
+        )
+    }
+}
+
+export default MyApp
