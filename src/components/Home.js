e4e6e661fc7bd276964a3d866b5be47423c74176
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2021-03-12 00:39:10 +0600

    Mapping State to Props

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,36 +1,24 @@
-import React,{Component} from 'react'
-import axios from 'axios'
-import {Link} from 'react-router-dom'
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 class Home extends Component {
 
-    state={
-        posts:[]
-    }
 
-  componentDidMount()
-  {
-      axios.get('https://jsonplaceholder.typicode.com/posts')
-      .then(res=>{
-          console.log(res)
-          this.setState({
-              posts: res.data.slice(0,10)
-          })
-      })
-  }
- 
+
     render() {
-        const {posts} = this.state
+        console.log(this.props)
+        const { posts } = this.props
 
         const postList = posts.length ? (
-            posts.map((post)=>{
-                return(
+            posts.map((post) => {
+                return (
                     <div className="post card center" key={post.id}>
-                       <div className="card-content"></div>
-                       <Link to={'/' + post.id}>
-                       <span className="card-title">{post.title}</span>
-                       </Link>
-                       <p>{post.body}</p>
+                        <div className="card-content"></div>
+                        <Link to={'/' + post.id}>
+                            <span className="card-title">{post.title}</span>
+                        </Link>
+                        <p>{post.body}</p>
                     </div>
                 )
             })
@@ -46,6 +34,11 @@ class Home extends Component {
     }
 
 }
-    
 
-export default Home
+const mapStateToProps = (state) => {
+    return {
+        posts: state.posts
+    }
+}
+
+export default connect(mapStateToProps)(Home)
